397acdc70f29922da920955b08885f5f93419b96
Alex Junior <alex.s.jr@outlook.com>
2022-12-12 09:03:55 -0300

    Criado form para o usuario atualizar seus dados

--- src/app/main/perfil/perfil.component.ts
+++ src/app/main/perfil/perfil.component.ts
@@ -1,6 +1,9 @@
 import { HttpHeaders } from '@angular/common/http';
+import { NonNullAssert } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
-import { Carro } from 'src/app/model/Carro';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AccountPut } from 'src/app/model/AccountPut';
 import { AccountService } from 'src/app/shared/account.service';
 import { AddressService } from 'src/app/shared/address.service';
 import { PedidoService } from 'src/app/shared/pedido.service';
@@ -47,20 +50,69 @@ export class PerfilComponent implements OnInit {
     carro: this.carro
   }
 
-  constructor(private addressService: AddressService, private accountService: AccountService, private pedidosService: PedidoService) {
-  }
-
   private id = window.localStorage.getItem('id')
+
+  atualizandoDados: boolean = false
+  perfilForm: FormGroup
+  usuarioModificando: AccountPut
+
+  constructor(
+    private addressService: AddressService,
+    private accountService: AccountService,
+    private pedidosService: PedidoService,
+    private fb: FormBuilder,
+    private router: Router) {
+  }
 
   header = new HttpHeaders()
     .set('Authorization', `Bearer ${this.accountService.getAuthorizationToken()}`)
+
+  atualizarDados(){
+    if(this.id != null){
+      this.usuarioModificando.id = parseInt(this.id)
+    }
+    this.usuarioModificando.nome = this.usuario.nomeUsuario
+    this.usuarioModificando.cpf = this.usuario.cpf
+    this.usuarioModificando.cnh = this.usuario.cnh
+    this.usuarioModificando.login = this.usuario.login
+    this.perfilForm = this.fb.group({
+      nome: new FormControl(this.usuarioModificando.nome),
+      cpf: new FormControl(this.usuarioModificando.cpf),
+      cnh: new FormControl(this.usuarioModificando.cnh),
+      email: new FormControl(this.usuarioModificando.login),
+      senha: new FormControl(this.usuarioModificando.senha)
+    })
+    this.atualizandoDados = true
+  }
+
+  onSubmit(){
+    if(this.id != null){
+      this.accountService.atualizarUsuario(parseInt(this.id), this.usuarioModificando, this.header).subscribe(
+        data =>{
+          console.log(data)
+          this.atualizandoDados = false
+          this.ngOnInit()
+        }
+      )
+    }
+  }
 
   ngOnInit(): void {
     if(this.id!=null && this.accountService.isUserLoggedIn()){
       this.accountService.getUsuarioAccount(this.id, this.header).subscribe(
         data=>{
           this.usuario = data
+          this.usuario.login = data.email
           console.log(this.usuario)
+          this.usuarioModificando = {
+            id: 0,
+            nome: '',
+            cpf: '',
+            cnh: '',
+            login: '',
+            senha: '',
+            admin: false
+          }
         })
 
         this.addressService.getAddress(this.id, this.header)
